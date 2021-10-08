0b793b6a9a597cc0bf836f0fcbeb8e06be716199
Riday ♡ <ridayshah2001@gmail.com>
2021-10-08 11:19:40 +0530

    🚧 Chore: request bar
    - The request bar component that will be used to make requests.

---
+++ src/components/ReqBar.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { methods } from '../util/data';
+import '../styles/request.scss';
+import ComboBox from './Combobox';
+
+const ReqBar = () => (
+	<div className='row' style={{ justifyContent: 'center' }}>
+		<div className='col-4 col-sm-3 col-md-2 col-xl-1'>
+			<ComboBox
+				id='method'
+				options={methods}
+				enableAutocomplete
+				placeholder='Method'
+				defaultValue='GET'
+				onChange={e => (e.target.value = e.target.value.toUpperCase())}
+				onBlur={e => console.log(e?.target.value)}
+			/>
+		</div>
+
+		<input className='c-input col-md-8 col-lg-4' />
+		<div className='col-md-4'>
+			<button>Submit</button>
+		</div>
+	</div>
+);
+
+export default ReqBar;
